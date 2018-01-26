5e799860b4d9130fcc70dfbbc693d8a61c9985c5
Aurelija <2266424U@student.gla.ac.uk>
2018-01-26 18:29:12 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category, Page
 
+#main page view:
 def index(request):
     #Construct a dictionary to pass to the template engine as its context
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupacke!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     #return a rendered response to send to the client
     #first param is the template we wish to use
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     #return HttpResponse("Rango says here is the about page <br/>"
     #                    "<a href='/rango/'>Index</a>")
     context_dict = {}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
 
 	url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
     url(r'^rango/', include('rango.urls')),
     # above maps uny URLs starting
     # with rango/ to be handled by
